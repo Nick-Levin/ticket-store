44f2c7b7375084b9b2157b8b0daa96a14ce2cd3e
valard <Kolya666kv@gmail.com>
2023-08-09 03:57:01 +0300

    added jacoco for test report

--- src/main/java/com/example/ticketstore/model/Ticket.java
+++ src/main/java/com/example/ticketstore/model/Ticket.java
@@ -1,6 +1,7 @@
 package com.example.ticketstore.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -8,6 +9,7 @@ import java.util.Date;
 
 @Data
 @Document("tickets")
+@EqualsAndHashCode(callSuper = true)
 public class Ticket extends AuditingBaseDocument {
 
     private String departurePlace;
